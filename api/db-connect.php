cebf1af77605f53108b5ecdc64178d36ba21f5be
Anastasiia Ovcharenko <you@example.com>
2020-11-09 22:10:56 +0200

    mysql connection

--- api/db-connect.php
+++ api/db-connect.php
@@ -1,2 +1,2 @@
 <?php
-$dbh = new PDO('mysql:host=localhost;dbname=streichholz', 'streichholz', 'ameholz');
+$dbh = new PDO('mysql:host=' . getenv('MYSQL_SERVER') . ';dbname=' . getenv('MYSQL_DB'), getenv('MYSQL_LOGIN'), getenv('MYSQL_PASSWORD'));
