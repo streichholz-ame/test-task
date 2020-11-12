9bc41de57ce53e1f87f15eb6c104a2712e9e77bf
Anastasiia Ovcharenko <you@example.com>
2020-11-13 00:00:10 +0200

    jquery ui + validation

--- api/add-list.php
+++ api/add-list.php
@@ -1,6 +1,10 @@
 <?php 
 require "db-connect.php";
 
+if (empty(trim($_POST['name']))) {
+    die('Name is empty!');
+}
+
 $data = $dbh->prepare('INSERT INTO ToDoList(name) VALUES (?)');
 
 $data->bindParam(1, $_POST['name']);
